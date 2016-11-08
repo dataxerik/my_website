ab93b07d3676c502c7e9c3dc2097a07aa25e7122
Xerik <dsshar@gmail.com>
2016-11-08 09:12:59 -0500

    Change static folder and start coding api call

--- wanikani/static/wanikani/index.js
+++ wanikani/static/wanikani/index.js
@@ -3,16 +3,19 @@ function resetPage() {
 }
 
 var apiHandler = {
-    interval: 10000,
+    interval: 5000,
+
 
     progressHandler: '',
 
     init: function() {
-        setTimeout(
+        this.getApiData()
+        //this.startProgressBar()
+        /*setTimeout(
             $.proxy(this.getApiData, this),
 
             this.intervalHandler
-            );
+            );*/
     },
 
     startProgressBar: function() {
@@ -25,27 +28,62 @@ var apiHandler = {
             return false;
         }
         cycleProgressBar();
-        //progressHandler = setInterval(function() {cycleProgressBar();}, 4000);
+        progressHandler = setInterval(function() {cycleProgressBar();}, 4000);
 
     },
 
     getApiData: function () {
-        var self = this;
-        this.startProgressBar();
-        self.init();
+        var url = 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information';
+        $.ajax( {url: 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information',
+                datatyp: 'jsonp',
+                success: function(data) {
+                console.log('yay')
+                }});
 
+                /*
+        .done(function(data) {
+            console.log("HIIIIIIIIII");
+            var data_ = data
+        })
+        .fail(function() {
+            console.log("error")
+        });
+        console.log("HIIIIIIIIIII")*/
+    /*
+        var self = this;
+        self.startProgressBar();
+        self.init();
+    */
     },
 }
+
+function getApiData() {
+        var url = 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information';
+        $.ajax( {url: 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information',
+                datatyp: 'jsonp',
+                success: function(data) {
+                console.log('yay')
+}})};
+
 function hideInput() {
         $("#api_key_box").css("display", "none");
     }
 
+function api(response) {
+    var foo = response.requested_information;
+    document.getElementById("output").innerHTML = foo.requested_information;
+    console.log(foo);
+};
 
 $(document).ready(function() {
     $("#progress").removeClass('running');
         $("input").click(function() {
             hideInput();
-            apiHandler.init();
+            getApiData();
+           /* var tag = document.createElement('script');
+            tag.src = 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information?callback=api';
+            document.getElementsByTagName("head")[0].appendChild(tag);*/
+            //apiHandler.init();
         });
 });
 
