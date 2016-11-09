06c91924f8ffa907567935c9564bee62349787be
dsharp <dsshar@gmail.com>
2016-11-09 02:22:04 -0500

    Updated  index.js to work for Cross-origin resource sharing for wanikani app

--- wanikani/static/wanikani/index.js
+++ wanikani/static/wanikani/index.js
@@ -1,93 +1,17 @@
-function resetPage() {
-
-}
-
-var apiHandler = {
-    interval: 5000,
-
-
-    progressHandler: '',
-
-    init: function() {
-        this.getApiData()
-        //this.startProgressBar()
-        /*setTimeout(
-            $.proxy(this.getApiData, this),
-
-            this.intervalHandler
-            );*/
-    },
-
-    startProgressBar: function() {
-        $("#progress").css("visibility", "visible");
-        function cycleProgressBar() {
-            $("#progress").removeClass('running').delay(10).queue(function(next) {
-                $(this).addClass('running');
-                next();
-             });
-            return false;
-        }
-        cycleProgressBar();
-        progressHandler = setInterval(function() {cycleProgressBar();}, 4000);
-
-    },
-
-    getApiData: function () {
-        var url = 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information';
-        $.ajax( {url: 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information',
-                datatyp: 'jsonp',
-                success: function(data) {
-                console.log('yay')
-                }});
-
-                /*
-        .done(function(data) {
-            console.log("HIIIIIIIIII");
-            var data_ = data
-        })
-        .fail(function() {
-            console.log("error")
-        });
-        console.log("HIIIIIIIIIII")*/
-    /*
-        var self = this;
-        self.startProgressBar();
-        self.init();
-    */
-    },
-}
-
-function getApiData() {
-        var url = 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information';
-        $.ajax( {url: 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information',
-                datatyp: 'jsonp',
-                success: function(data) {
-                console.log('yay')
-}})};
+function foo(response) {
+    document.getElementById('output').innerHTML = response.user_information.username
+};
 
 function hideInput() {
         $("#api_key_box").css("display", "none");
-    }
-
-function api(response) {
-    var foo = response.requested_information;
-    document.getElementById("output").innerHTML = foo.requested_information;
-    console.log(foo);
-};
+}
 
 $(document).ready(function() {
     $("#progress").removeClass('running');
         $("input").click(function() {
             hideInput();
-            getApiData();
-           /* var tag = document.createElement('script');
-            tag.src = 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information?callback=api';
-            document.getElementsByTagName("head")[0].appendChild(tag);*/
-            //apiHandler.init();
+            var tag = document.createElement("script");
+            tag.src = 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information?callback=foo';
+            document.getElementsByTagName("head")[0].appendChild(tag);
         });
 });
-
-/*
-$(document).ready(function() {
-    alert("Hi");
-});*/
