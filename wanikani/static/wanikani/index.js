0378b94bffa41e8d76750a428afa344423224372
Xerik <dsshar@gmail.com>
2016-11-09 03:42:36 -0500

    Added another way to call wanikani api with jsonp

--- wanikani/static/wanikani/index.js
+++ wanikani/static/wanikani/index.js
@@ -6,12 +6,52 @@ function hideInput() {
         $("#api_key_box").css("display", "none");
 }
 
+function callWanikaniApi() {
+    var dfd = $.Deferred();
+
+    function startProgressBar() {
+        $("#progress").css("visibility", "visible");
+        function cycleProgressBar() {
+            $("#progress").removeClass('running').delay(10).queue(function(next) {
+                $(this).addClass('running');
+                next();
+             });
+            return false;
+        }
+        cycleProgressBar();
+        progressHandler = setInterval(function() {cycleProgressBar();}, 4000);
+    }
+    startProgressBar();
+    var tag = document.createElement("script");
+    tag.src = 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information?callback=foo';
+
+    document.getElementsByTagName("head")[0].appendChild(tag);
+
+}
+
+function callWanikaniApi2() {
+    $.ajax({
+        url: 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information',
+        type: 'GET',
+        dataType: 'jsonp',
+        beforeSend: function() {
+            console.log("HIIII")
+            $("#progress").removeClass('running').delay(10).queue(function(next) {
+                $(this).addClass('running');
+                next();
+            });
+        },
+        success: function(data) {
+            console.log(data);
+            $(document).get("head").append(data);
+        },
+    });
+}
+
 $(document).ready(function() {
     $("#progress").removeClass('running');
         $("input").click(function() {
             hideInput();
-            var tag = document.createElement("script");
-            tag.src = 'https://www.wanikani.com/api/user/c9d088f9a75b0648b3904ebee3d8d5fa/user-information?callback=foo';
-            document.getElementsByTagName("head")[0].appendChild(tag);
+            callWanikaniApi2();
         });
 });
