2a0875b2e225124637ebd8c6a3a1c19e3f316941
dsharp <dsshar@gmail.com>
2017-11-28 00:30:12 -0500

    added project section and about section to home page, added nwe style sheet and js for home page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 from . import views
 
-
+app_name='home'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'projects/$', TemplateView.as_view(template_name='projects.html')),
 ]
