6da8fffb6d2278828da7af90a3b71f31d0352be0
Xerik <dsshar@gmail.com>
2018-01-30 11:35:19 -0500

    Added more project templates, pictures, and some basic formatting to base template

--- home/urls.py
+++ home/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     url(r'resume/$', TemplateView.as_view(template_name='home/resume.html'), name='resume'),
     url(r'wanikani_project/$', TemplateView.as_view(template_name='home/projects/projects_wanikani.html'),
         name='wanikani_project'),
+    url(r'puzzler_project/$', TemplateView.as_view(template_name='home/projects/udacity_vr/projects_puzzler.html'),
+        name='puzzler_project'),
+    url(r'night_at_the_museum_project/$',
+        TemplateView.as_view(template_name='home/projects/udacity_vr/projects_night_at_the_museum.html'),name='night_at_the_museum_project'),
 ]
