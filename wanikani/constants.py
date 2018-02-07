7e8e021fbae55329fd54075f1c8bc785673c1898
Xerik <dsshar@gmail.com>
2018-02-07 09:41:48 -0500

    change path for text files and added a new error message

--- wanikani/constants.py
+++ wanikani/constants.py
@@ -1,10 +1,10 @@
-JLPT_KANJI_FILE_LOCATIONS = {'N4': 'wanikani\static\wanikani\jlpt\jlpt4.txt',
-                             'N1': 'wanikani\static\wanikani\jlpt\jlpt1.txt',
-                             'N2': 'wanikani\static\wanikani\jlpt\jlpt2.txt',
-                             'N3': 'wanikani\static\wanikani\jlpt\jlpt3.txt',
-                             'N5': 'wanikani\static\wanikani\jlpt\jlpt5.txt'}
+JLPT_KANJI_FILE_LOCATIONS = {'N4': 'wanikani/static/wanikani/jlpt/jlpt4.txt',
+                             'N1': 'wanikani/static/wanikani/jlpt/jlpt1.txt',
+                             'N2': 'wanikani/static/wanikani/jlpt/jlpt2.txt',
+                             'N3': 'wanikani/static/wanikani/jlpt/jlpt3.txt',
+                             'N5': 'wanikani/static/wanikani/jlpt/jlpt5.txt'}
 
-JLPT_KANJI_FILE_LOCATION = 'wanikani\static\wanikani\jlpt\jlpt.txt'
+JLPT_KANJI_FILE_LOCATION = 'wanikani/static/wanikani/jlpt/jlpt.txt'
 
 JLPT_KANJI_LEVELS = ['N5', 'N4', 'N3', 'N2', 'N1']
 
@@ -16,15 +16,15 @@ PROGRESS_KANJI_LEVELS = ['JLPT_KANJI_LEVELS', 'JOYO_KANJI_LEVELS', 'FREQUENCY_KA
 
 PROGRESS_TYPES = ['JLPT', 'JOYO', 'FREQUENCY']
 
-JOYO_KANJI_FILE_LOCATIONS = 'wanikani\static\wanikani\joyo\joyo.txt'
+JOYO_KANJI_FILE_LOCATIONS = 'wanikani/static/wanikani/joyo/joyo.txt'
 
-FREQUENCY_KANJI_FILE_LOCATIONS = {'F1': 'wanikani\static\wanikani\\frequency\\tangorin_1015.csv',
-                                  'F2': 'wanikani\static\wanikani\\frequency\\tangorin_1016.csv',
-                                  'F3': 'wanikani\static\wanikani\\frequency\\tangorin_1017.csv',
-                                  'F4': 'wanikani\static\wanikani\\frequency\\tangorin_1018.csv',
-                                  'F5': 'wanikani\static\wanikani\\frequency\\tangorin_1019.csv'}
+FREQUENCY_KANJI_FILE_LOCATIONS = {'F1': 'wanikani/static/wanikani/frequency/tangorin_1015.csv',
+                                  'F2': 'wanikani/static/wanikani/frequency/tangorin_1016.csv',
+                                  'F3': 'wanikani/static/wanikani/frequency/tangorin_1017.csv',
+                                  'F4': 'wanikani/static/wanikani/frequency/tangorin_1018.csv',
+                                  'F5': 'wanikani/static/wanikani/frequency/tangorin_1019.csv'}
 
-FREQUENCY_KANJI_FILE_LOCATION = 'wanikani\static\wanikani\\frequency\\frequency.txt'
+FREQUENCY_KANJI_FILE_LOCATION = 'wanikani/static/wanikani/frequency/frequency.txt'
 
 
 SRS_APPRENTICE_LEVEL = "apprentice"
--- wanikani/error/error_text_constants.py
+++ wanikani/error/error_text_constants.py
@@ -2,6 +2,7 @@ API_KEY_404 = "Could\'t find the API key. Please re-enter the API key."
 API_KEY_MALFORMED = "The given API key isn't valid. Please enter a valid API key."
 API_KEY_BAD_CALL = "While calling the WaniKani service, there was an issue. " \
                   "Please double check the API key and try again."
+BAD_REQUEST_RESPONSE = "There was an issue while calling the Wanikani api. Please wait and try again later."
 class APIErrorMessage:
     @staticmethod
     def construct_api_error_message(message):
